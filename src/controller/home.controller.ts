a42281accef42317934522cb5d4030e5dfae0cee
liangjiayu <1013834609@qq.com>
2025-08-03 16:52:40 +0800

    chore: 部署配置修改

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -8,8 +8,10 @@ export class HomeController {
 
   @Get('*')
   async home(): Promise<string> {
+    /**
+     * 可根据实际情况获取静态资源，如判断域名
+     */
     const result = await this.httpService.get('http://cdn.liangjiayu.top/react-admin-vite/prod/index.html');
-
     return result.data;
   }
 }
