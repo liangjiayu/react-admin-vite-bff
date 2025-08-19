e9413a2fda9ea16ab9b910ad1f2c6a8cda12e5db
liangjiayu <1013834609@qq.com>
2025-08-19 22:44:05 +0800

    chore: update home controller with ETag support

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,17 +1,30 @@
 import { HttpService } from '@midwayjs/axios';
 import { Controller, Get, Inject } from '@midwayjs/core';
+import { Context } from '@midwayjs/koa';
 
 @Controller('/')
 export class HomeController {
   @Inject()
   httpService: HttpService;
 
+  @Inject()
+  ctx: Context;
+
   @Get('*')
   async home(): Promise<string> {
     /**
      * 可根据实际情况获取静态资源，如判断域名
      */
-    const result = await this.httpService.get('http://cdn.liangjiayu.top/react-admin-vite/prod/index.html');
-    return result.data;
+    const cdnUrl = 'http://cdn.liangjiayu.top/react-admin-vite/prod/index.html';
+    const response = await this.httpService.get(cdnUrl, {
+      responseType: 'text',
+    });
+
+    // 用于前端判断是否需要更新
+    if (response.headers.etag) {
+      this.ctx.set('ETag', response.headers.etag);
+    }
+
+    return response.data;
   }
 }
